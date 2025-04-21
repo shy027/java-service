6d7635c042b62a3de75cc94ade563787738024f8
shy <1533103845@qq.com>
2025-04-21 15:52:15 +0800

    第一版

---
+++ src/main/java/top/shy/javaservice/JavaServiceApplication.java
@@ -0,0 +1,23 @@
+package top.shy.javaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class JavaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServiceApplication.class, args);
+    }
+    @GetMapping("/list")
+    public List<String> getList(){
+        return List.of("java","javaScript","Python");
+    }
+}
+
+
